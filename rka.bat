dcca1cc3eab36ac66f19690d8fea089cdd7f8d13
N <nespambox@gmail.com>
2023-05-21 14:50:40 +0200

    remove unnecessary batch files

--- rka.bat
+++
@@ -1,8 +0,0 @@
-if exist C:\storage\share\rka_logs (
-    venv\Scripts\python -m rka.app.starter > C:\storage\share\rka_logs\server-%computername%.txt
-) else if exist D:\storage\share\rka_logs (
-    venv\Scripts\python -m rka.app.starter > D:\storage\share\rka_logs\server-%computername%.txt
-) else (
-    git pull github master
-    venv_vm\Scripts\python -m rka.app.starter
-)
